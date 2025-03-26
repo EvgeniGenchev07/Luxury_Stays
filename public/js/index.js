a7826615c6be599af77be22f8ebfd3f4ea81c78e
Evgeni Genchev <120590298+EvgeniGenchev07@users.noreply.github.com>
2025-03-26 23:10:58 +0200

    ui fixes

--- public/js/index.js
+++ public/js/index.js
@@ -292,3 +292,15 @@ function calculateDaysBetween(fromDate, toDate) {
 
     return Math.abs(diffInDays); // Return the absolute value to avoid negatives
 }
+function toggleNext() {
+    var element = document.getElementById('leftandin1');
+    element.classList.toggle('delay-06s');
+    element.classList.toggle('fadeout');
+    element.classList.toggle('displaynone');
+    var element = document.getElementById('leftandin2');
+    element.classList.toggle('display');
+    element.classList.toggle('animated');
+    var element = document.getElementById('inner2');
+    element.classList.toggle('delay-06s');
+    element.classList.toggle('fadeIn');
+}
